ac2d9cffcecd687ffc4efe75b19bf21d18772d00
Osama Khallouf <50551721+Exeedo@users.noreply.github.com>
2022-08-30 20:57:34 +0300

    Create BFS.cpp

---
+++ Graph Theory/BFS.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue> // for queue and priority queue
+using namespace std;
+
+int V, e;
+vector<vector<int>> adj;
+vector<int> dist;
+
+void traversal(int s) {
+	dist[s] = 0;
+	queue<int> q;
+	q.push(s);
+	while (!q.empty()) {
+		int u = q.front();
+		q.pop();
+		for (int v : adj[u]) {
+			if (dist[v] == -1) {
+				dist[v] = dist[u] + 1; // for unweighted graph
+				q.push(v);
+			}
+		}
+	}
+}
+
+int main() {
+	cin >> V >> e;
+	adj.resize(V+1);
+	dist.resize(V+1);
+	for (int i = 1; i <= V; i++)
+		dist[i] = -1;
+	for (int i = 0; i < e; i++) {
+		int u, v, w;
+		cin >> u >> v >> w; // weight is not used for BFS
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	traversal(1);
+	for (int i = 1; i <= V; i++)
+		cout << "dist of " << i << " = " << dist[i] << endl;
+}
